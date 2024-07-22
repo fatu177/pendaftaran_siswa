6ebe8f16b625937fddfac9828aae99607b2f671d
fatu177 <fatu.dzaki@gmail.com>
2024-07-22 14:07:29 +0700

    buat faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,47 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\User;
+use App\Models\Gelombang;
+use App\Models\Jurusan;
+use App\Models\pesertapelatihan;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-        $faker = 
-for ($i=0; $i < 20; $i++) {
-    User::create([
-        'name' => $faker->name,
-        'email' => 'test@example.com',
-    ]);
-}
+        $faker = Faker::create('id_ID'); // Adjust locale to 'id_ID' for Indonesian locale
+
+        // Fetch all jurusan and gelombang IDs
+        $jurusanIds = Jurusan::pluck('id')->toArray();
+        $gelombangIds = Gelombang::pluck('id')->toArray();
+
+        for ($i = 0; $i < 20; $i++) {
+            $name = $faker->name;
+            pesertapelatihan::create([
+                'nama_lengkap' => $name,
+                'email' => $faker->email,
+                'id_jurusan' => $faker->randomElement($jurusanIds),
+                'id_gelombang' => $faker->randomElement($gelombangIds),
+                'nik' => $faker->nik,
+                'kartu_keluarga' => $faker->nik,
+                'jenis_kelamin' => $faker->randomElement(['Laki - Laki', 'Perempuan']),
+                'tempat_lahir' => $faker->city,
+                'tanggal_lahir' => $faker->date,
+                'pendidikan_terakhir' => $faker->randomElement(['S-1', 'SMA', 'S-2']),
+                'nama_sekolah' => $faker->company,
+                'kejuruan' => $faker->randomElement(['Fisika', 'Komputer', 'Bahasa Inggris']),
+                'nomor_hp' => $faker->phoneNumber,
+                'aktivitas_saat_ini' => $faker->randomElement(['Mencari Kerja', 'Sekolah', 'Bekerja']),
+                'status' => $faker->numberBetween(0, 2),
+            ]);
+        }
     }
 }
